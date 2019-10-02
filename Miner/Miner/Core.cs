49dbe8f02dbeb846ce2a5315977bb53e5f245c24
ArleKuin <36620710+dart647@users.noreply.github.com>
2019-10-03 02:06:27 +0300

    Игра сапер
    Сделаны основные функции игры

---
+++ Miner/Miner/Core.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Miner
+{
+    class Core: Form1
+    {
+        protected void defeat()
+        {
+            //Поражение
+            foreach (FieldButton button in fb)
+            {
+                if (button.Bomb())
+                {
+                    if (!button.isClicable)
+                        button.Text = "▲*";
+                    else
+                        button.Text = "*";
+                }
+                else
+                {
+                    if (!button.isClicable && button.isFlag)
+                        button.Text = "x▲";
+                }
+            }
+
+            DialogResult res = MessageBox.Show("Вы проиграли!\nНовая игра?",
+                                            "Поражение",
+                                            MessageBoxButtons.YesNo,
+                                            MessageBoxIcon.Exclamation);
+
+            if (res == DialogResult.Yes)
+            {
+                Application.Restart();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        protected void victory()
+        {
+            //Победа
+            DialogResult res = MessageBox.Show("Вы выиграли!\nНовая игра?",
+                            "Победа",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Asterisk);
+
+            if (res == DialogResult.Yes)
+            {
+                Application.Restart();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        protected void setFlag(FieldButton bt)
+        {
+            //поставить или снять флажок
+            if (bt.isClicable)
+            {
+                if (FieldButton.bombCount > 0)
+                {
+                    bt.Text = "▲";
+                    bt.isClicable = false;
+                    bt.isFlag = true;
+                    FieldButton.bombCount--;
+                    FieldCount--;
+                    tb.Text = FieldButton.bombCount.ToString();
+                }
+            }
+            else
+            {
+                bt.Text = "";
+                bt.isClicable = true;
+                bt.isFlag = false;
+                FieldButton.bombCount++;
+                FieldCount++;
+                tb.Text = FieldButton.bombCount.ToString();
+            }
+        }
+
+        protected void openRegion(FieldButton bt)
+        {
+            //Открывает все 0 ячейки в округе, если нажата ячейка с 0
+            var str = bt.Name.Split(' ');
+            var x1 = Convert.ToInt32(str[0]) - bWidth;
+            var x2 = x1 + bWidth * 3;
+            var y1 = Convert.ToInt32(str[1]) - bHight;
+            var y2 = y1 + bHight * 3;
+
+            var buttons = new List<FieldButton>();
+
+            for (int i = x1; i < x2; i += bWidth)
+            {
+                for (int j = y1; j < y2; j += bHight)
+                {
+                    FieldButton tempBt = fb.Find(button => button.Name == $"{i} {j}");
+                    if (tempBt != null && tempBt.isClicable && !tempBt.Bomb())
+                    {
+                        int b = bombAround(tempBt);
+                        openCell(tempBt, b);
+                        if (b == 0)
+                            buttons.Add(tempBt);
+                    }
+                }
+            }
+
+            foreach (FieldButton button in buttons)
+            {
+                openRegion(button);
+            }
+        }
+
+        protected void openCell(FieldButton bt, int b)
+        {
+            //открыть ячейку
+            bt.FlatStyle = FlatStyle.Popup;
+            bt.Text = b.ToString();
+            FieldCount--;
+            bt.Enabled = false;
+            bt.isClicable = false;
+        }
+
+        protected int bombAround(FieldButton bt)
+        {
+            //Считает бомбы вокруг нажатой кнопки
+            int bombs = 0;
+            var str = bt.Name.Split(' ');
+            var x1 = Convert.ToInt32(str[0]) - bWidth;
+            var x2 = x1 + bWidth * 3;
+            var y1 = Convert.ToInt32(str[1]) - bHight;
+            var y2 = y1 + bHight * 3;
+
+            for (int i = x1; i < x2; i += bWidth)
+            {
+                for (int j = y1; j < y2; j += bHight)
+                {
+                    FieldButton tempBt = fb.Find(button => button.Name == $"{i} {j}");
+                    if (tempBt != null && tempBt.Bomb())
+                        bombs++;
+                }
+            }
+            return bombs;
+        }
+
+        public void Bt_Click(object sender, MouseEventArgs e)
+        {
+            var button = sender as FieldButton;//нажатая кнопка
+            if (e.Button == MouseButtons.Right)//флажки
+            {
+                setFlag(button);
+            }
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (button.isClicable)
+                {
+                    if (button.Bomb())
+                        defeat();//поражение
+                    else
+                    {
+                        int b = bombAround(button);
+                        openCell(button, b);//открытие ячеек
+                        if (b == 0)
+                        {
+                            openRegion(button);
+                        }
+                    }
+                }
+            }
+
+            if (FieldCount == 0)
+                victory();//победа
+        }
+
+        public void Bt_GotFocus(object sender, EventArgs e)
+        {
+            ActiveForm.Focus();
+        }
+    }
+}
---
+++ Miner/Miner/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Miner
+{
+    public partial class Form1 : Form
+    {
+        protected static List<FieldButton> fb = new List<FieldButton>();
+        protected static TextBox tb = new TextBox();
+        protected static int FieldCount = 0;
+        protected static int bWidth = 30;
+        protected static int bHight = 30;
+        protected static int x = 0;
+        protected static int y = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            newGame();
+        }
+
+        protected void newGame()
+        {
+            //Запуск игры
+            FieldButton.bombCount = 0;
+            createField();
+            Focus();
+        }
+
+        protected int setBombs()
+        {
+            int maxBombs = 48;
+            Random rng = new Random();
+            while (FieldButton.bombCount < maxBombs)
+                foreach (FieldButton bt in fb)
+                {
+                    int bombsInLine = 3;
+                    if (rng.Next(0, 101) < FieldButton.bombPercent && FieldButton.bombCount < maxBombs && bombsInLine > 0)
+                    {
+                        bt.isBomb = true;
+                       // bt.Text = "*";
+                        bombsInLine--;
+                        FieldButton.bombCount++;
+                    }
+                }
+            return FieldButton.bombCount;
+        }
+
+        protected void createField()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    x = 15 + bWidth * j;
+                    y = 45 + bHight * i;
+                    FieldButton bt = new FieldButton();
+                    bt.Size = new Size(bWidth, bHight);
+                    bt.Location = new Point(x, y);
+                    bt.Name = $"{x} {y}";
+                    bt.Text = "";
+                    tb.TabIndex = 1;
+
+                    fb.Add(bt);
+                    Controls.Add(bt);
+
+                    bt.MouseUp += new Core().Bt_Click;
+                    bt.GotFocus += new Core().Bt_GotFocus;
+                }
+            }
+
+            FieldCount = fb.Count();
+
+            tb.Size = new Size(bWidth, bHight);
+            tb.Location = new Point(15, 15);
+            tb.Text = setBombs().ToString();
+            tb.ReadOnly = true;
+            tb.Enabled = false;
+            tb.TabIndex = 0;
+            Controls.Add(tb);
+        }
+    }
+
+    public class FieldButton: Button
+    {
+        public bool isBomb = false;
+        public static double bombPercent = 20;
+        public static int bombCount = 0;
+        public bool isClicable = true;
+        public bool isFlag = false;
+
+        public bool Bomb()
+        {
+            return isBomb;
+        }
+    }
+}
